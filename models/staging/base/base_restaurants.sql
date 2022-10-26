9ea0206680316539631adc5334a59b02c53bec5c
jgros@ippon.fr <jgros@ippon.fr>
2022-10-26 10:22:39 +0000

    Update source restaurants

--- models/staging/base/base_restaurants.sql
+++ models/staging/base/base_restaurants.sql
@@ -4,5 +4,5 @@ SELECT
     address, 
     nb_emplyees, 
     open_on_sunday
-FROM {{ mockable_source('public', 'restaurants', 'sample__restaurants') }}
+FROM {{ mockable_source('source', 'restaurants', 'sample__restaurants') }}
 qualify row_number() over (partition by identifier order by identifier) = 1
